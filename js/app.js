8df4e00b6c5cba97122d0bcf7b88584a020d3248
Ksenia C <ksenia@Ksenias-MacBook-Pro.local>
2022-02-18 14:34:10 -0500

    added service worker and IDB

--- js/app.js
+++ js/app.js
@@ -1,10 +1,13 @@
 
 const APP = {
     DB: null,  //the indexedDB
+    DBVersion: 1,
+    searchStore: null,
+    suggestStore: null,
     isONLINE: 'onLine' in navigator && navigator.onLine,
-    tmdbBASEURL: '',
-    tmdbAPIKEY: '',
-    tmdbIMAGEBASEURL: '',
+    tmdbBASEURL: 'https://api.themoviedb.org/3/',
+    tmdbAPIKEY: '99bfb48bf74bed439d4a45a53a55f249',
+    tmdbIMAGEBASEURL: 'https://image.tmdb.org/t/p/w1280',
     results: [],
     init: ()=>{
         //when the page loads
@@ -14,37 +17,127 @@ const APP = {
     },
     registerSW: ()=>{
         //register the service worker
-
+        // COPIED
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('/sw.js').catch(function (err) {
+              // Something went wrong during registration. The sw.js file
+              // might be unavailable or contain a syntax error.
+                console.warn(err);
+            });
+            navigator.serviceWorker.ready.then((registration) => {
+              // .ready will never reject... just wait indefinitely
+                APP.sw = registration.active;
+              //save the reference to use later or use .ready again
+            });
+        }
+        // END COPIED
+        
         //then add listeners and run page specific code
         APP.pageSpecific();
         APP.addListeners();
     },
+    
     openDatabase: (nextStep)=>{
         //open the database
+        let dbOpenRequest = indexedDB.open('movieDB', APP.DBVersion);
+
         //add the update, error, success listeners in here
-        //call nextStep onsuccess
-        //create searchStore with keyword as keyPath
-        //create suggestStore with movieid as keyPath
+        dbOpenRequest.onupgradeneeded = function (ev) {
+            console.log('upgrade needed')
+            APP.DB = ev.target.result;
+
+            try {
+            APP.DB.deleteObjectStore('searchStore');
+            APP.DB.deleteObjectStore('suggestStore');
+            } catch (err) {
+                console.log('err deleting older db')
+            }
+
+            //create searchStore with keyword as keyPath
+            APP.searchStore = APP.DB.createObjectStore("searchStore", {keyPath: "keyword", autoIncrement: false,});
+            APP.searchStore.createIndex("by_title", "title", { unique: false });
+            //create suggestStore with movieid as keyPath
+            APP.suggestStore = APP.DB.createObjectStore("suggestStore", {keyPath: "movieid", autoIncrement: false,});
+            APP.suggestStore.createIndex("by_title", "title", { unique: false });
+        }
+        
+        dbOpenRequest.onerror = function (err) {
+            console.log(err.message);
+         //an error has happened during opening
+        };
+
+        dbOpenRequest.onsuccess = function (ev) {
+            APP.DB = dbOpenRequest.result;
+            //or ev.target.result
+            //result will be the reference to the database that you will use
+            console.log(APP.DB.oldVersion)
+            console.log(APP.DB.name);
+            console.log(APP.DB.version);
+            console.log(APP.DB.objectStoreNames);
+            console.log(APP.DB.name, `is ready to be used.`); 
+            //call nextStep onsuccess
+            nextStep()
+        };
     },
+    
     createTransaction: (storeName)=>{
-        let tx;
-        //create a transaction to use for some interaction with the database
-        return tx;
+        let isDataAdded = localStorage.getItem(`MDB${APP.DBVersion}`);
+
+        if (!isDataAdded) { //the data is not there
+            let tx = APP.DB.transaction(storeName, 'readwrite');
+            console.log({ tx });
+
+            APP.addResultsToDB(tx, storeName, index);
+            tx.oncomplete = function (ev) {
+                console.log('All movies added');
+                localStorage.setItem(`MDB${APP.DBVersion}`, true)
+            }
+            return tx
+        } else {
+        } 
     },
     getDBResults: (storeName, keyValue) => {
         //return the results from storeName where it matches keyValue
     },
-    addResultsToDB: (obj, storeName)=>{
+    addResultsToDB: (obj, storeName, index)=>{
         //pass in the name of the store
         //save the obj passed in to the appropriate store
     },
     addListeners: ()=>{
-        //add listeners
-        //when the search form is submitted
-        //when clicking on the list of possible searches on home or 404 page
-        //when a message is received
-        //when online and offline
+        //add event listeners for DOM
+        
+        //check if already installed
+        if (navigator.standalone) {
+            console.log('Launched: Installed (iOS)');
+            APP.isStandalone = true;
+        } else if (matchMedia('(display-mode: standalone)').matches) {
+            console.log('Launched: Installed');
+            APP.isStandalone = true;
+        } else {
+            // console.log('Launched: Browser Tab');
+            APP.isStandalone = false;
+        }
+    
+        //add event listeners for online and offline
+        window.addEventListener('online', APP.changeOnlineStatus);
+        window.addEventListener('offline', APP.changeOnlineStatus);
+    
+        //add listener for message
+        navigator.serviceWorker.addEventListener('message', APP.messageReceived);
+    
+        //add listener for install event
+        window.addEventListener('beforeinstallprompt', (ev) => {
+            // Prevent the mini-infobar from appearing on mobile
+            ev.preventDefault();
+            // Save the event in a global property
+            // so that it can be triggered later.
+            APP.deferredPrompt = ev;
+            console.log('deferredPrompt saved');
+            // Build your own enhanced install experience
+            // use the APP.deferredPrompt saved event
+        });
     },
+
     pageSpecific:()=>{
         //anything that happens specifically on each page
         if(document.body.id === 'home'){
@@ -64,7 +157,25 @@ const APP = {
     },
     changeOnlineStatus: (ev)=>{
         //when the browser goes online or offline
+        APP.isONLINE = ev.type === 'online' ? true : false;
+        //TODO: send message to sw about being online or offline
+        navigator.serviceWorker.ready.then((registration) => {
+        registration.active.postMessage({ ONLINE: APP.isONLINE});
+    });
+
+    APP.changeDisplay();
     },
+
+    changeDisplay: () => {
+        if (APP.isONLINE) {
+            //online
+            
+        } else {
+            //offline
+            document.body.style.background ='#000000'
+        }
+    },
+
     messageReceived: (ev)=>{
         //ev.data
     },
@@ -133,11 +244,3 @@ const APP = {
 }
 
 document.addEventListener('DOMContentLoaded', APP.init);
-
-class NetworkError extends Error {
-    constructor(msg, status, statusText){
-        super(msg);
-        this.status = status;
-        this.statusText = statusText;
-    }
-}
--- sw.js
+++ sw.js
@@ -1,28 +1,101 @@
 const version = 1;
-const staticName = `PWA-Static-Movie-APP-${version}`;
-const dynamicName = `PWA-Dynamic-Movie-APP-${version}`;
+const staticCache = `PWA-Static-Movie-APP-${version}`;
+const dynamicCache = `PWA-Dynamic-Movie-APP-${version}`;
 const cacheLimit = 100;
 const cacheList = [
     '/',
     '/index.html',
+    '/404.html',
+    '/results.html',
+    '/suggested.html',
+    '/css/main.css',
+    '/js/app.js',
+    '/manifest.json',
+    '/favicon.png',
+    '/img/404.webp',
+    '/img/offline.webp',
+    '/img/android-chrome-192x192.png',
+    '/img/android-chrome-512x512.png',
+    '/img/apple-touch-icon.png',
+    '/img/favicon-16x16.png',
+    '/img/favicon-32x32.png',
+    '/img/placeholder-img.png',
+    'https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap',
+    'https://fonts.googleapis.com/icon?family=Material+Icons'
 ];
 
-self.addEventListener('install', (ev)=>{
-    ev.waitUntil();
+self.addEventListener('install', (ev) => {
+    ev.waitUntil(
+        caches.open(staticCache).then((cache) => {
+        //save the whole cacheList
+        cache.addAll(cacheList);
+        })
+    );
 });
-self.addEventListener('activate', (ev)=>{
-    ev.waitUntil();
-});
-self.addEventListener('fetch', (ev)=>{
-    ev.respondWith();
-});
-self.addEventListener('message', (ev)=>{
-    //check ev.data to get the message
+self.addEventListener('activate', (ev) => {
+    ev.waitUntil(
+        caches
+        .keys()
+        .then((keys) => {
+            return Promise.all(
+            keys
+                .filter((key) => {
+                if (key === staticCache || key === dynamicCache) {
+                    return false;
+                } else {
+                    return true;
+                }
+                })
+                .map((key) => caches.delete(key))
+          ); //keys.filter().map() returns an array of Promises
+        })
+        .catch(console.warn)
+    );
 });
 
-function sendMessage(msg){
-    //send a message to the browser from the service worker
-};
+self.addEventListener('fetch', (ev) => {
+    ev.respondWith(
+        caches.match(ev.request).then((cacheRes) => {
+            return (
+            cacheRes ||
+            fetch(ev.request)
+                .then((fetchRes) => {
+                //TODO: check here for the 404 error
+                if (!fetchRes.ok) throw new Error(fetchRes.statusText);
+    
+                return caches.open(dynamicCache).then((cache) => {
+                    let copy = fetchRes.clone(); //make a copy of the response
+                    cache.put(ev.request, copy); //put the copy into the cache
+                    return fetchRes; //send the original response back up the chain
+                });
+                })
+                .catch((err) => {
+                    console.log('SW fetch failed');
+                    console.warn(err);
+                    if (ev.request.mode == 'navigate') {
+                        //send the 404 page
+                        return caches.match('/404.html').then((page404Response) => {
+                        return page404Response;
+                        });
+                    }
+                })
+            );
+        })
+    ); //what do we want to send to the browser?
+});
+
+
+function sendMessage(msg) {
+    //send a message to the browser
+    //from the service Worker
+    //code from messaging.js Client API send message code
+    self.clients.matchAll().then(function (clients) {
+        if (clients && clients.length) {
+        //Respond to last focused tab
+        clients[0].postMessage(msg);
+        }
+    });
+}
 
 function limitCache(){
     //remove some files from the dynamic cache
